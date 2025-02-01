6e1a0dbbf306b657abaf2b10936c848ad601f790
idkwhttsay <danil.tatarinov.00@gmail.com>
2025-02-01 12:53:58 -0500

    feat: add review model and admin functionality

--- movies/admin.py
+++ movies/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from .models import Movie
+from .models import Movie, Review
+
 
 class MovieAdmin(admin.ModelAdmin):
     ordering = ['name']
     search_fields = ['name']
 
+class ReviewAdmin(admin.ModelAdmin):
+    ordering = ['data']
+    search_fields = ['comment', 'data']
+
 admin.site.register(Movie, MovieAdmin)
+admin.site.register(Review, ReviewAdmin)
