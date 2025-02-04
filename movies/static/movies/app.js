7330b5de3e40dd2d76c7a52c5063f941ebeb2727
calvin-k347 <clark.calvink@gmail.com>
2025-02-04 02:41:24 -0500

    MVS-18 feat: updated animations and theme

--- movies/static/movies/app.js
+++ movies/static/movies/app.js
@@ -1,3 +1,24 @@
-function getColor() {
-    
-}
+const search = document.getElementById("search");
+const movieNodes = document.querySelectorAll(".movie-node");
+const movies = Array.from(movieNodes).map((node, index) => {
+    return { name: node.dataset.name.toLowerCase(), element: node };
+});
+search.addEventListener("input", () => {
+   searching = search.value.toLowerCase().trim()
+    const rankedMovies = movies.map(movie => {
+        let score = 0;
+        if (movie.name.startsWith(searching)) {
+            score += 1;
+        }
+        if (movie.name.includes(searching)) {
+            score += .5;
+        }
+        return {...movie,score};
+    }).sort((a,b) => b.score - a.score);
+   const child = document.querySelector(".child");
+   child.innerHTML = "";
+   rankedMovies.forEach((movie, i) => {
+       movie.element.classList.add("added")
+       child.appendChild(movie.element);
+   });
+});
