0e10535a170cdbea91e5b8149d68f9c3f49fe536
JIAHAO CHEN <100472232@alumnos.uc3m.es>
2025-02-05 23:27:44 -0500

    Added forgot_password user story

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('signup/', views.signup, name='accounts.signup'),
     path('login/', views.login, name='accounts.login'),
     path('logout/', views.logout, name='accounts.logout'),
+    path('forgot-password/', views.forgot_password, name='accounts.forgot_password'),
+    path('reset-password/', views.reset_password, name='accounts.reset_password'),
 ]
