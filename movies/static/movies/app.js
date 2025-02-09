e99786f25909c58033432850985617676f4373ed
calvin-k347 <clark.calvink@gmail.com>
2025-02-09 17:58:59 -0500

    feat: fade animation to shows in movies tab

--- movies/static/movies/app.js
+++ movies/static/movies/app.js
@@ -52,5 +52,18 @@ search.addEventListener("focus", () =>{
     popular.style.animation = "smooth-widen forwards .2s ease-in"
     popular.style.right = "4vh"
 
-
 });
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+    });
+});
+const hidden = document.querySelectorAll(".hidden");
+hidden.forEach((element) => observer.observe(element));
+
